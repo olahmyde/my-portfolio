b63b9b8e918fc44f200e39f99d208d7021682ba0
olahmyde <37003274+olahmyde@users.noreply.github.com>
2023-04-15 03:37:17 +0100

    Nav button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
-import ME from '../../assets/me.png';
+import ME from '../../assets/tech.png';
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
     <header>
       <div className='container header__container'>
-        <h5> Hello I'm</h5>
+        <h5 id='home'> Hello I'm</h5>
         <h1>Olamide Olufade</h1>
         <h5 className='text-light'> Web Developer / IT Consultant / Business Manager</h5>
         <CTA />
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import './nav.css';
+import {AiOutlineHome} from 'react-icons/ai'; 
+import {AiOutlineUser} from 'react-icons/ai';
+import {ImBooks} from 'react-icons/im'; 
+import {MdMiscellaneousServices} from 'react-icons/md'; 
+import {MdOutlineContacts} from 'react-icons/md';
 
 const Nav = () => {
+  const [usedbtn, setUsedbtn] = useState('nav--inactive');
   return (
-    <div>Nav</div>
+    <nav>
+      <a className={usedbtn === 'nav--inactive' ?  'nav-active' : 'nav--inactive'} href='#home'> <AiOutlineHome/></a>
+      <a className={setUsedbtn} href='#about'> <AiOutlineUser/></a>
+      <a className={setUsedbtn} href='#experience'> <ImBooks/></a>
+      <a className={setUsedbtn} href='#services'> <MdMiscellaneousServices/></a>
+      <a className={setUsedbtn} href='#contact'> <MdOutlineContacts/></a>
+    </nav>
   )
 }
 
