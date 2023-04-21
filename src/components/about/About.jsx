e4bb7d94d8b91d0225194c477757daa4f4481bc4
olahmyde <37003274+olahmyde@users.noreply.github.com>
2023-04-21 23:06:07 +0100

    New update
    Updated the experience and about components

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import './about.css';
+import techie from '../../assets/techie.png';
+import {FaAward} from 'react-icons/fa';
+import {FiUsers} from 'react-icons/fi';
+import {VscFolderActive} from 'react-icons/vsc';
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5 id='about'> Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img className="about--image" src={techie} alt='About Me'/>
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            
+            <article className="about__card">
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years working</small>
+            </article>
+            
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>50+ Worldwide</small>
+            </article>
+            
+            <article className="about__card">
+              <VscFolderActive className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ Completed projects</small>
+            </article>
+            
+          </div>
+        
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum cumque sed magni voluptas, qui amet tempora laudantium repellat eaque! Assumenda nostrum nobis pariatur, eaque praesentium tenetur qui obcaecati autem aliquid?
+          </p>
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
