6a05592e81825b86055b7fa96e9d34e1cbb810b9
olahmyde <37003274+olahmyde@users.noreply.github.com>
2023-04-24 03:33:28 +0100

    Fully responsive portfolio website
    Completed and ready to be deployed on a website.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
 import './contact.css';
+import {MdOutlineMail} from 'react-icons/md';
+import {SlSocialInstagram} from 'react-icons/sl';
+import {BsWhatsapp} from 'react-icons/bs';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 
 const Contact = () => {
+  
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_f44xv4s', 'template_j3j8ilo', form.current, 'BociOcLZNbziM2imF')
+    .then((result) => {
+        console.log(result.text);
+        window.location.reload();
+    }, (error) => {
+        console.log(error.text);
+    });
+    
+    e.target.reset();
+  };
+  
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Form</h2>
+      
+      <div className="container contact__container">
+        <div className="contact__options">
+          
+          <article className="contact__option">
+            <MdOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>olamidelalaolufade@gmail.com</h5>
+            <a href="mailto:olamidelalaolufade@gmail.com" target='_blank' rel="noreferrer"> Send an Email</a>
+          </article>
+          
+          <article className="contact__option">
+            <SlSocialInstagram className='contact__option-icon'/>
+            <h4>Instagram</h4>
+            <h5>olamide.olufade</h5>
+            <a href="https://instagram.com/olamide.olufade" target='_blank' rel="noreferrer"> Send a DM</a>
+          </article>
+          
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+44 7******</h5>
+            <a href="https://api.whatsapp.com/send?phone+447404438945" target='_blank' rel="noreferrer"> Send a Message</a>
+          </article>
+          
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type='text' name='name' placeholder='First name and last name' required />
+          <input type='email' name='email' placeholder='Enter your email' required />
+          <textarea name='message' rows='7' placeholder='Your message' required></textarea>
+          <div className='contact__btn'>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+          </div>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import './footer.css';
+import {FaFacebookF} from 'react-icons/fa';
+import {BsInstagram} from 'react-icons/bs';
+import {BsTwitter} from 'react-icons/bs';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#home' className='footer__logo'> OLAMIDE OLUFADE</a>
+      <ul className='permalinks'>
+        <li><a href='#home'> Home</a></li>
+        <li><a href='#about'> About</a></li>
+        <li><a href='#experience'> Experience</a></li>
+        <li><a href='#services'> Services</a></li>
+        <li><a href='#portfolio'> Portfolio</a></li>
+        <li><a href='#testimonials'> Testimonials</a></li>
+        <li><a href='#contact'> Contact</a></li>
+      </ul>
+      
+      <div className="footer__socials">
+        <a href='https://facebook.com'><FaFacebookF/></a>
+        <a href='https://instagram.com'><BsInstagram/></a>
+        <a href='https://twitter.com'><BsTwitter/></a>
+      </div>
+      
+      <div className="footer__copyright">
+        <small>&copy; Olamide Olufade. All rights reserved. </small>
+      </div>
+    </footer>
   )
 }
 
